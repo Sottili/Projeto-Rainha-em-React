49bb82b01fc2d9d06e9dc214cf035487ca7ebca6
Felipe Sottili <felipesottili32@gmail.com>
2023-02-21 19:22:55 -0300

    alterações no quem somos

--- src/templates/aboutUs/index.jsx
+++ src/templates/aboutUs/index.jsx
@@ -24,11 +24,11 @@ const AboutUs = () => {
             <h1 className="title-aboutUs">Quem somos?</h1>
           </Col>
           <Col id="col-text-home" xs={12}>
-            <Col xxl={3}>
+            <Col xxl={2}>
               <img className="img-fluid" width={320} src={gloriaFrighetto} alt="Gloria Frighetto Dirigente" />
             </Col>
             <Col className="mt-4" style={{ textAlign: 'center' }} xxl={4}>
-              <h3>Gloria Strega</h3>
+              <h2 style={{ marginBottom: '30px' }}>Gloria Strega</h2>
               <p className="paragraph-aboutUs">
                 Gloria Strega, terapeuta desde 2010, fundou o Céu Rainha da Nova Era em outubro de 2015 com o objetivo
                 de levar uma nova consciência de vida para pessoas desde planeta. Buscando reconexão com a natureza,
@@ -39,7 +39,7 @@ const AboutUs = () => {
               </p>
             </Col>
             <Col className="mt-4" style={{ textAlign: 'center' }} xxl={4}>
-              <h3>Oque fazemos?</h3>
+              <h2 style={{ marginBottom: '30px' }}>Oque fazemos?</h2>
               <p className="paragraph-aboutUs">
                 Nossa principal atividade são os trabalhos na linhagem do Neo Xamanismo Universal, com a utilização das
                 Medicinas da Floresta como Ayahuasca, Rapé, Sananga, Tabaco, Kambô, Tenda do suor, dentre outras.
@@ -53,10 +53,10 @@ const AboutUs = () => {
             <Col className="title-localization" xs={12} xl={12} xxl={12}>
               <h2>Onde estamos localizados?</h2>
             </Col>
-            <Col xs={12} xl={3} xxl={3}>
+            <Col className="colImgAbout" xs={12} xl={3} xxl={3} md={12}>
               <img width={320} className="img-fluid" src={fotoCasa} alt="Foto da Rainha da Antiga religião" />
             </Col>
-            <Col xs={12} style={{ textAlign: 'center', marginBottom: '30px' }} xl={3} xxl={3}>
+            <Col className="colParagraphsAboutUs" xs={12} xl={3} xxl={3} md={12}>
               <p className="paragraph-aboutUs">
                 Estamos Localizados próximos as estações de Metrô Tatuapé, Belém e Vila Prudente. Da estação até nossa
                 casa leva em torno de 20 minutos de ônibus. Uber (Tatuapé ou Belém) custa aproximadamente R$ 20,00. Uber
